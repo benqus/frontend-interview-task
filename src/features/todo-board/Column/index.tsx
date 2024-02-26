656ef6242172c0b3334223acbd4c3057a90c55a7
Bence Kormos <ben.kormos@gmail.com>
2024-02-26 19:26:19 +0100

    run pnpm format

--- src/features/todo-board/Column/index.tsx
+++ src/features/todo-board/Column/index.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 import styles from "./styles.module.css";
 
-type Props = { children?: ReactNode, name: string, };
+type Props = { children?: ReactNode; name: string };
 
 export function Column({ children, name }: Props) {
   return (
